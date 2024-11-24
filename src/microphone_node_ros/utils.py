8c9f1ead520c4e5e22fe110902f2e3798472f9a1
certafonso <certafonso@gmail.com>
2024-11-24 18:39:55 +0100

    Add config file to add new microphones

--- src/microphone_node_ros/utils.py
+++ src/microphone_node_ros/utils.py
@@ -1,67 +1,36 @@
 import pyaudio
+from rospkg import RosPack
+import yaml
 
 def open_audio_stream(microphone_in_use, sample_rate, frame_length):
+    # Get the list of possible microphones
+    config_path = f"{RosPack().get_path('microphone_node')}/config/mic_config.yaml"
+    
+    with open(config_path, 'r') as f:
+        config = yaml.safe_load(f)
+
+    # Initialize PyAudio
     paudio = pyaudio.PyAudio()
     info = paudio.get_host_api_info_by_index(0)
     numdevices = info.get('deviceCount')
 
-    respeaker_mic_id = None
-    azure_mic_id = None
-    rode_mic_id = None
-    for i in range(0, numdevices):
-        # print("---")
-        # print(i, paudio.get_device_info_by_host_api_device_index(0, i).get('name'))
-        if (paudio.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-            if 'ReSpeaker' in paudio.get_device_info_by_host_api_device_index(0, i).get('name'):
-                respeaker_mic_id = i
+    # Find the specified microphone device
+    mic_id = None
+    mic_name = config.get(microphone_in_use)
+    if mic_name is not None:
 
-            elif 'Azure' in paudio.get_device_info_by_host_api_device_index(0, i).get('name'):
-                azure_mic_id = i
+        # Iterate through all devices and find the one with the specified name
+        for i in range(0, numdevices):
+            # print("---")
+            # print(i, paudio.get_device_info_by_host_api_device_index(0, i).get('name'))
 
-            elif 'USB Audio Device' in paudio.get_device_info_by_host_api_device_index(0, i).get('name'):
-                rode_mic_id = i
+            if (paudio.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+                if mic_name in paudio.get_device_info_by_host_api_device_index(0, i).get('name'):
+                    mic_id= i
+                    break
 
-    # Open audio stream
-    if microphone_in_use == "respeaker":
-        if respeaker_mic_id == None:
-            raise Exception('Cannot find ReSpeaker input channel.')
-        
-        audio_stream = paudio.open(
-            rate=sample_rate,
-            channels=1,
-            format=pyaudio.paInt16,
-            input=True,
-            frames_per_buffer=frame_length,
-            input_device_index=respeaker_mic_id,
-            )
-        
-    elif microphone_in_use == "azure":
-        if azure_mic_id == None:
-            raise Exception('Cannot find Azure input channel.')
-        
-        audio_stream = paudio.open(
-            rate=sample_rate,
-            channels=1,
-            format=pyaudio.paInt16,
-            input=True,
-            frames_per_buffer=frame_length,
-            input_device_index=azure_mic_id,
-            )
-        
-    elif microphone_in_use == "rode":
-        if rode_mic_id == None:
-            raise Exception('Cannot find Rode input channel.')
-
-        audio_stream = paudio.open(
-            rate=sample_rate,
-            channels=1,
-            format=pyaudio.paInt16,
-            input=True,
-            frames_per_buffer=frame_length,
-            input_device_index=rode_mic_id,
-            )
-        
-    elif microphone_in_use == "default":
+    # Open default microphone
+    if microphone_in_use == "default":
         audio_stream = paudio.open(
             rate=sample_rate,
             channels=1,
@@ -70,7 +39,18 @@ def open_audio_stream(microphone_in_use, sample_rate, frame_length):
             frames_per_buffer=frame_length,
             )
         
+    # Open audio stream of non default microphone
     else:
-        raise Exception(f'{microphone_in_use} is not a valid microphone.')
+        if mic_id == None:
+            raise Exception(f"Cannot find {microphone_in_use} input channel.")
+        
+        audio_stream = paudio.open(
+            rate=sample_rate,
+            channels=1,
+            format=pyaudio.paInt16,
+            input=True,
+            frames_per_buffer=frame_length,
+            input_device_index=mic_id,
+            )
     
     return audio_stream
